e193374371e1c92f8cbfe87e26770d1a84f69af3
mfmarano <mmarano@frba.utn.edu.ar>
2024-04-02 16:16:54 -0300

    feat(client): send CLAVE and package

--- client/main.go
+++ client/main.go
@@ -3,24 +3,19 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger()
 
-	// loggear "Hola soy un log" usando la biblioteca log
 	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
-	// validar que la config este cargada correctamente
 
-	// loggeamos el valor de la config
+	if globals.ClientConfig == nil {
+		log.Fatalf("No se pudo cargar la configuración")
+	}
 
-	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
+	utils.EnviarMensaje(globals.ClientConfig.Ip, globals.ClientConfig.Puerto, globals.ClientConfig.Mensaje)
 
-	// enviar un mensaje al servidor con el valor de la config
-
-	// leer de la consola el mensaje
-	// utils.LeerConsola()
-
-	// generamos un paquete y lo enviamos al servidor
-	// utils.GenerarYEnviarPaquete()
+	utils.GenerarYEnviarPaquete()
 }
